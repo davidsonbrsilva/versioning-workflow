a20b9c62b6ed4e268ec01df333665c68fe8758fc
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-25 04:44:45 -0300

    fix: filtered get last version to ignore 'v' pattern

--- actions/latest-version-obtaining/latest-version-obtaining.sh
+++ actions/latest-version-obtaining/latest-version-obtaining.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Fetch tags from remote.
-git fetch --unshallow origin --tags > /dev/null 2>&1 || { echo "Failed to fetch tags from remote."; exit 1; }
+git fetch origin --tags > /dev/null 2>&1 || { echo "Failed to fetch tags from remote."; exit 1; }
 
 # Get the last repository tag.
-last_version=$(git describe --tags --abbrev=0 2>/dev/null)
+last_version=$(git tag --sort=version:refname | grep -v '^v' | tail -n 1)
 
 if [ -z "$last_version" ]; then
   echo "No found version associated with this branch."
