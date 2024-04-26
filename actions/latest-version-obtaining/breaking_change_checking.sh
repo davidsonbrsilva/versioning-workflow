5eaec9bdd0bdfad0e7199674f262cac8a1184888
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-26 10:10:35 -0300

    feat: added breaking change checking step

---
+++ actions/latest-version-obtaining/breaking_change_checking.sh
@@ -0,0 +1,18 @@
+regex="([0-9]+)\.([0-9]+)\.([0-9]+)"
+
+if [[ $VERSION =~ $regex ]]; then
+  major_version="${BASH_REMATCH[1]}"
+  minor_version="${BASH_REMATCH[2]}"
+  patch_version="${BASH_REMATCH[3]}"
+
+  if [[ $major_version > 0 ]]; then
+    if [[ $minor_version == 0 && $patch_version == 0 ]]; then
+      echo "is_breaking_change=true" >> $GITHUB_OUTPUT
+      echo "The last found version is breaking change."
+      exit 0
+    fi
+  fi
+fi
+
+echo "The last found version is not breaking change."
+exit 0
