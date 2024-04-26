b26a90f74308bf94436930c624f52365a3c732e5
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-26 17:05:36 -0300

    fix: fixed listing tags

--- actions/latest-version-obtaining/latest-version-obtaining.sh
+++ actions/latest-version-obtaining/latest-version-obtaining.sh
@@ -9,7 +9,7 @@ fi
 git fetch --unshallow origin --tags > /dev/null 2>&1 || { echo "Failed to fetch tags from remote."; exit 1; }
 
 # Get the last repository tag.
-last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "$ORIGIN_BRANCH") | grep -v '^v' | tail -n 1)
+last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "origin/$ORIGIN_BRANCH") | grep -v '^v' | tail -n 1)
 
 if [[ -z "$last_version" ]]; then
   echo "No found version associated with this branch."
