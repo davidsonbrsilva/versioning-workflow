c303b96ddbaba6a6ddf93865cf147d963bc5b60a
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-26 16:15:00 -0300

    fix: removed unshallow command

--- actions/latest-version-obtaining/latest-version-obtaining.sh
+++ actions/latest-version-obtaining/latest-version-obtaining.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Fetch tags from remote.
-git fetch --unshallow origin --tags > /dev/null 2>&1 || { echo "Failed to fetch tags from remote."; exit 1; }
+git fetch origin --tags > /dev/null 2>&1 || { echo "Failed to fetch tags from remote."; exit 1; }
 
 # Get the last repository tag.
-last_version=$(git tag --sort=committerdate | grep -v '^v' | tail -n 1)
+last_version=$(git tag --sort=committerdate --merged=$(git rev-parse HEAD) | grep -v '^v' | tail -n 1)
 
-if [ -z "$last_version" ]; then
+if [[ -z "$last_version" ]]; then
   echo "No found version associated with this branch."
   exit 0
 fi
