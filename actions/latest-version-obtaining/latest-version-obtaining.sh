70fec1fd8cb2f5477cfd8a3d04ddb103ac03db03
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-26 17:51:45 -0300

    temp: added debug logs

--- actions/latest-version-obtaining/latest-version-obtaining.sh
+++ actions/latest-version-obtaining/latest-version-obtaining.sh
@@ -12,8 +12,18 @@ git fetch --unshallow origin --tags > /dev/null 2>&1 || { echo "Failed to fetch 
 origin_branch_last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "origin/$ORIGIN_BRANCH") | grep -v '^v' | tail -n 1)
 main_branch_last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "origin/main") | grep -v '^v' | tail -n 1)
 
+echo "Origin branch last version: $origin_branch_last_version"
+echo "Main branch last version: $main_branch_last_version"
+
 last_version=$(echo -e "$origin_branch_last_version\n$main_branch_last_version" | sort -V | tail -n 1)
-last_version=$(git tag -l "$last_version*" | sort -V | tail -n 1)
+
+echo "Choosed version: $last_version"
+
+existing_tags=$(git tag -l "$last_version*")
+
+echo "Existing tags: $existing_tags"
+
+last_version=$($existing_tags | sort -V | tail -n 1)
 
 if [[ -z "$last_version" ]]; then
   echo "No found version associated with this branch."
