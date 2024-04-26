50e29d9ddda9bd46aceb035e7c76f31a2277be6a
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-26 17:39:21 -0300

    feat: added treatment for origin/main last version

--- actions/latest-version-obtaining/latest-version-obtaining.sh
+++ actions/latest-version-obtaining/latest-version-obtaining.sh
@@ -9,7 +9,10 @@ fi
 git fetch --unshallow origin --tags > /dev/null 2>&1 || { echo "Failed to fetch tags from remote."; exit 1; }
 
 # Get the last repository tag.
-last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "origin/$ORIGIN_BRANCH") | grep -v '^v' | tail -n 1)
+origin_branch_last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "origin/$ORIGIN_BRANCH") | grep -v '^v' | tail -n 1)
+main_branch_last_version=$(git tag --sort=committerdate --merged=$(git rev-parse "origin/main") | grep -v '^v' | tail -n 1)
+
+last_version=$(echo -e "$origin_branch_last_version\n$main_branch_last_version" | sort -V | tail -n 1)
 last_version=$(git tag -l "$last_version*" | sort -V | tail -n 1)
 
 if [[ -z "$last_version" ]]; then
