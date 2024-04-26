e86f943fc12b60a30ce1f3b77c7843ec85b9c71d
Davidson Bruno <davidsonbruno@outlook.com>
2024-04-26 18:29:45 -0300

    feat: added treatment for add rc suffix

--- actions/latest-version-obtaining/latest-version-obtaining.sh
+++ actions/latest-version-obtaining/latest-version-obtaining.sh
@@ -19,8 +19,6 @@ last_version=$(echo -e "$origin_branch_last_version\n$main_branch_last_version" 
 
 echo "Choosed version: $last_version"
 
-last_version=$(git tag -l "$last_version*" | sort -V | tail -n 1)
-
 if [[ -z "$last_version" ]]; then
   echo "No found version associated with this branch."
   exit 0
