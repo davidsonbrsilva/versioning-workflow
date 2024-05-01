edafee2617b344e2268ec315b831b190b2cad4f3
Davidson Bruno <davidsonbruno@outlook.com>
2024-05-01 04:41:44 -0300

    refactor: improved readability for breaking change script

--- actions/latest-version-obtaining/breaking_change_checking.sh
+++ actions/latest-version-obtaining/breaking_change_checking.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-regex="([0-9]+)\.([0-9]+)\.([0-9]+)"
+readonly VERSION
 
-if [[ $VERSION =~ $regex ]]; then
-  major_version="${BASH_REMATCH[1]}"
-  minor_version="${BASH_REMATCH[2]}"
-  patch_version="${BASH_REMATCH[3]}"
+is_breaking_change() {
+  [[ "${1}" -gt 0 && "${2}" -eq 0 && "${3}" -eq 0 ]]
+}
 
-  if [[ $major_version > 0 ]]; then
-    if [[ $minor_version == 0 && $patch_version == 0 ]]; then
-      echo "is_breaking_change=true" >> $GITHUB_OUTPUT
-      echo "The last found version is breaking change."
+is_valid_version() {
+  [[ "${1}" =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]
+}
+
+check() {
+  local version="${1}"
+  readonly version
+
+  if is_valid_version "${version}"; then
+    local major_version="${BASH_REMATCH[1]}"
+    local minor_version="${BASH_REMATCH[2]}"
+    local patch_version="${BASH_REMATCH[3]}"
+
+    if is_breaking_change "${major_version}" "${minor_version}" "${patch_version}"; then
+      echo "is_breaking_change=true" >> "${GITHUB_OUTPUT}"
+      printf "Version %s is breaking change.\n" "${version}"
       exit 0
     fi
   fi
-fi
 
-echo "The last found version is not breaking change."
-exit 0
+  printf "Version %s is not breaking change.\n" "${version}"
+  exit 0
+}
+
+check "${VERSION}"
