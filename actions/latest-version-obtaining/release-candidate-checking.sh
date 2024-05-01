8c22c2649411a30deeb6a3dc160d0ce7af8298b0
Davidson Bruno <davidsonbruno@outlook.com>
2024-05-01 19:02:41 -0300

    refactor: improved readability for rc checking

--- actions/latest-version-obtaining/release-candidate-checking.sh
+++ actions/latest-version-obtaining/release-candidate-checking.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-regex="([0-9]+)\.([0-9]+)\.([0-9]+)-rc-([0-9]+)"
+readonly VERSION
 
-if [[ $VERSION =~ $regex ]]; then
-  echo "is_release_candidate=true" >> $GITHUB_OUTPUT
-  echo "The last version found is release candidate."
-else
-  echo "The last version found is not release candidate."
-fi
+is_release_candidate() {
+  [[ "${1}" =~ ([0-9]+)\.([0-9]+)\.([0-9]+)-rc-([0-9]+) ]]
+}
+
+check() {
+  if is_release_candidate "${1}"; then
+    printf "is_release_candidate=true" >> "${GITHUB_OUTPUT}"
+    printf "The last version found is release candidate.\n"
+    exit 0
+  fi
+
+  printf "The last version found is not release candidate."
+  exit 0
+}
+
+check "${VERSION}"
